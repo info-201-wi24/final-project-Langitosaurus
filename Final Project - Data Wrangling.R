cae74b2fca0cd2237f11e775bd0f14bb07bbde65
Langitosaurus <skektongkol@gmail.com>
2024-02-17 14:28:55 -0800

    dataset addition

--- Final Project - Data Wrangling.R
+++ Final Project - Data Wrangling.R
@@ -1,1 +1,6 @@
-#Testing
+library("tidyverse")
+
+xpr_df <- read.csv("datasets/xrp.csv")
+nasdaq_df <- read.csv("datasets/nasdaq.csv")
+
+#Joining based on date, need new column labels to differentiate xpr and nasdaq
