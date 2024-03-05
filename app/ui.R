03cadd77f2e98cd919b47dd849f550aa712d5e10
Langitosaurus <skektongkol@gmail.com>
2024-03-04 23:00:01 -0800

    theme select creation; app ui testing

--- app/ui.R
+++ app/ui.R
@@ -1,25 +1,23 @@
-
-
 ## OVERVIEW TAB INFO
 
-overview_tab <- tabPanel("Overview Tab Title",
-   h1("Some title"),
-   p("some explanation")
+overview_tab <- tabPanel("Project Overview",
+   h1("Crypto : Nasdaq"),
+   p("With the rise of cryptocurrency as an investment model, it is")
 )
 
 ## VIZ 1 TAB INFO
 
 viz_1_sidebar <- sidebarPanel(
-  h2("Options for graph"),
-  #TODO: Put inputs for modifying graph here
+  h2("Scale by Time"),
+  h2("Data Selection"),
 )
 
 viz_1_main_panel <- mainPanel(
-  h2("Vizualization 1 Title"),
-  # plotlyOutput(outputId = "your_viz_1_output_id")
+  h2("Comparative Price by Time"),
+  plotlyOutput(outputId = "output_1")
 )
 
-viz_1_tab <- tabPanel("Viz 1 tab title",
+viz_1_tab <- tabPanel("Price Plot",
   sidebarLayout(
     viz_1_sidebar,
     viz_1_main_panel
@@ -29,16 +27,16 @@ viz_1_tab <- tabPanel("Viz 1 tab title",
 ## VIZ 2 TAB INFO
 
 viz_2_sidebar <- sidebarPanel(
-  h2("Options for graph"),
-  #TODO: Put inputs for modifying graph here
+  h2("Scale by Time"),
+  h2("Data Selection"),
 )
 
 viz_2_main_panel <- mainPanel(
-  h2("Vizualization 2 Title"),
+  h2("Fluctuation of Price Change by Time"),
   # plotlyOutput(outputId = "your_viz_1_output_id")
 )
 
-viz_2_tab <- tabPanel("Viz 2 tab title",
+viz_2_tab <- tabPanel("Change Plot",
   sidebarLayout(
     viz_2_sidebar,
     viz_2_main_panel
@@ -57,7 +55,7 @@ viz_3_main_panel <- mainPanel(
   # plotlyOutput(outputId = "your_viz_1_output_id")
 )
 
-viz_3_tab <- tabPanel("Viz 3 tab title",
+viz_3_tab <- tabPanel("",
   sidebarLayout(
     viz_3_sidebar,
     viz_3_main_panel
@@ -66,8 +64,8 @@ viz_3_tab <- tabPanel("Viz 3 tab title",
 
 ## CONCLUSIONS TAB INFO
 
-conclusion_tab <- tabPanel("Conclusion Tab Title",
- h1("Some title"),
+conclusion_tab <- tabPanel("In Conclusion",
+ h1("Comparability of Cryptocurrency"),
  p("some conclusions")
 )
 
