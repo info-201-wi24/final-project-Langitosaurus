c0422b057a287929d1a1a252636872c0c38ceb2c
AjitMallav <108138617+AjitMallav@users.noreply.github.com>
2024-03-05 16:32:18 -0800

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -1,80 +1,82 @@
-## OVERVIEW TAB INFO
+library(plotly)
+library(bslib)
+library(dplyr)
+library(markdown)
 
-overview_tab <- tabPanel("Project Overview",
-   h1("Crypto : Nasdaq"),
-   p("With the rise of cryptocurrency as an investment model, it is")
+pastel_blue <- "#ADD8E6"
+
+# Introduction Page
+introduction_tab <- tabPanel("Introduction",
+                             h1("Welcome to Crypto Nasdaq Analysis", style = "color: #333; font-family: Georgia, serif; text-align: center;"),
+                             p("This dashboard provides insights into cryptocurrency trends and market analysis.", style = "color: #333; font-family: Georgia, serif;")
 )
 
-## VIZ 1 TAB INFO
-
+# Visualization 1 Page
 viz_1_sidebar <- sidebarPanel(
-  h2("Scale by Time"),
-  h2("Data Selection"),
+  h2("Scale by Time", style = "color: #333; font-family: Georgia, serif; text-align: center;"),
+  h2("Data Selection", style = "color: #333; font-family: Georgia, serif; text-align: center;")
 )
 
 viz_1_main_panel <- mainPanel(
-  h2("Comparative Price by Time"),
+  h2("Comparative Price by Time", style = "color: #333; font-family: Georgia, serif; text-align: center;"),
   plotlyOutput(outputId = "output_1")
 )
 
 viz_1_tab <- tabPanel("Price Plot",
-  sidebarLayout(
-    viz_1_sidebar,
-    viz_1_main_panel
-  )
+                      sidebarLayout(
+                        viz_1_sidebar,
+                        viz_1_main_panel
+                      )
 )
 
-## VIZ 2 TAB INFO
-
+# Visualization 2 Page
 viz_2_sidebar <- sidebarPanel(
-  h2("Scale by Time"),
-  h2("Data Selection"),
+  h2("Scale by Time", style = "color: #333; font-family: Georgia, serif; text-align: center;"),
+  h2("Data Selection", style = "color: #333; font-family: Georgia, serif; text-align: center;")
 )
 
 viz_2_main_panel <- mainPanel(
-  h2("Fluctuation of Price Change by Time"),
-  # plotlyOutput(outputId = "your_viz_1_output_id")
+  h2("Fluctuation of Price Change by Time", style = "color: #333; font-family: Georgia, serif; text-align: center;"),
+  plotlyOutput(outputId = "your_viz_1_output_id")
 )
 
 viz_2_tab <- tabPanel("Change Plot",
-  sidebarLayout(
-    viz_2_sidebar,
-    viz_2_main_panel
-  )
+                      sidebarLayout(
+                        viz_2_sidebar,
+                        viz_2_main_panel
+                      )
 )
 
-## VIZ 3 TAB INFO
-
+# Visualization 3 Page
 viz_3_sidebar <- sidebarPanel(
-  h2("Options for graph"),
-  #TODO: Put inputs for modifying graph here
+  h2("Options for graph", style = "color: #333; font-family: Georgia, serif; text-align: center;")
+  # TODO: Put inputs for modifying graph here
 )
 
 viz_3_main_panel <- mainPanel(
-  h2("Vizualization 3 Title"),
-  # plotlyOutput(outputId = "your_viz_1_output_id")
+  h2("Visualization 3 Title", style = "color: #333; font-family: Georgia, serif; text-align: center;")
+  plotlyOutput(outputId = "your_viz_1_output_id")
 )
 
 viz_3_tab <- tabPanel("",
-  sidebarLayout(
-    viz_3_sidebar,
-    viz_3_main_panel
-  )
+                      sidebarLayout(
+                        viz_3_sidebar,
+                        viz_3_main_panel
+                      )
 )
 
-## CONCLUSIONS TAB INFO
-
-conclusion_tab <- tabPanel("In Conclusion",
- h1("Comparability of Cryptocurrency"),
- p("some conclusions")
+# Conclusion Page
+conclusion_tab <- tabPanel("Conclusion",
+                           h1("Summary and Conclusion", style = "color: #333; font-family: Georgia, serif; text-align: center;"),
+                           p("In this analysis, we explored the comparability of cryptocurrencies and their trends.", style = "color: #333; font-family: Georgia, serif;")
 )
 
-
-
-ui <- navbarPage("Example Project Title",
-  overview_tab,
-  viz_1_tab,
-  viz_2_tab,
-  viz_3_tab,
-  conclusion_tab
+# UI
+ui <- navbarPage("Crypto Nasdaq Analysis",
+                 introduction_tab,
+                 viz_1_tab,
+                 viz_2_tab,
+                 viz_3_tab,
+                 conclusion_tab,
+                 tags$body(style = paste0("background-color: ", pastel_blue, ";"))
 )
