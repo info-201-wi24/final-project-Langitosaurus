1d74edee8fd0c33ff7ab472e0dd4bcc5c6bc590a
Langitosaurus <skektongkol@gmail.com>
2024-03-05 16:56:06 -0800

    fixed dataset inconsistency (date reversed)

--- Final Project - Data Wrangling.R
+++ Final Project - Data Wrangling.R
@@ -9,7 +9,7 @@ xpr_df$Date <- as.Date(xpr_df$Date, "%m/%d/%Y")
 nasdaq_df$Date <- as.Date(nasdaq_df$Date, "%m/%d/%Y")
 
 #Joining based on date, only closing prices used.
-joined_df <- left_join(xpr_df 
+joined_df <- left_join(xpr_df
                        %>% rename(xpr_price = Price) 
                        %>% select(Date,xpr_price), 
                        nasdaq_df 
@@ -20,7 +20,7 @@ joined_df <- left_join(xpr_df
 joined_df <- joined_df %>% filter(!is.na(nasdaq_price))
 
 #calculate change from each type of data
-joined_df <- joined_df %>% 
+joined_df <- joined_df %>% arrange(Date) %>%  
   mutate(xpr_change = xpr_price - lag(xpr_price)) %>% mutate(nasdaq_change = nasdaq_price - lag(nasdaq_price))
 
 #Categorical variable indicating whether the XRP price increased, decreased, or stayed the same 
